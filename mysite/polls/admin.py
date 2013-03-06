42680668a21e3c27f8d3facae1e58a0b528af64d
jess <jess@jess.(none)>
2013-03-06 22:32:01 +0000

    Hecho hasta el cambio de aspecto de Poll

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,11 +2,16 @@ from polls.models import Poll
 from polls.models import Choice
 from django.contrib import admin
 
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
 class PollAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None, 		{'fields': ['question']}),
 		('Fecha', 	{'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
+	inlines = [ChoiceInline]
+	list_display = ('question', 'pub_date') #Si aniadimos was_published_today salta un error de que la variable global datetime no esta definida.
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
